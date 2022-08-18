4425b7cea1c5f7d6979ade0f2307ef1ddd8f1219
Michele <eien.tabi@outlook.it>
2022-08-19 01:55:46 +0200

    Adjusted code readibility.

--- Patch.cs
+++ Patch.cs
@@ -1,9 +1,11 @@
 ﻿using UnityEngine;
 using HarmonyLib;
+using Entitas;
 using PhantomBrigade;
 using PhantomBrigade.Data;
 using System.Collections.Generic;
 using PhantomBrigade.Combat.Systems;
+using System;
 
 namespace fragmentMod
 {
@@ -14,92 +16,87 @@ namespace fragmentMod
         [HarmonyPatch(typeof(ScheduledAttackSystem), "Execute", MethodType.Normal)]
 
         [HarmonyPostfix]
-        static void Sas_fragment(List <CombatEntity> entities, CombatEntity projectile, CombatContext combat)
+        static void Sas_fragment(List <CombatEntity> entities)
         {
 
-            for (int i = 0; i < entities.Count; i++)
+            foreach (var projectile in Contexts.sharedInstance.combat.GetEntities(CombatMatcher.DataLinkSubsystemProjectile))
             {
-                projectile = entities[i];
-                combat = Contexts.sharedInstance.combat;
-
-                Vector3 firingPoint = Vector3.one;
-                Vector3 targetPoint = Vector3.MoveTowards(Vector3.zero, Vector3.forward, 1.0f);
-                Vector3 bodyAssetScale = Vector3.Scale(Vector3.zero, Vector3.one);
-                Transform scatteredDirection = null;
-                var bak = CombatComponentsLookup.AssetKey;
-                var level = CombatComponentsLookup.Level;
-                var parentAction = ActionComponentsLookup.ActionOwner;
-                bool bodyAssetUsed = true;
+                CombatContext combat = Contexts.sharedInstance.combat;
 
                 var part = IDUtility.GetEquipmentEntity(projectile.parentPart.equipmentID);
                 var subsystem = IDUtility.GetEquipmentEntity(projectile.parentSubsystem.equipmentID);
                 var blueprint = subsystem.dataLinkSubsystem.data;
                 var fragmentDelayFound = blueprint.TryGetFloat("fragment_delay", out float fragmentDelay, 0);
                 var fragmentKeyFound = blueprint.TryGetString("fragment_key", out string fragmentKey, null);
-                var fragmentBlueprint = DataMultiLinkerSubsystem.GetEntry(fragmentKey);
-                var projectileData = fragmentBlueprint.projectileProcessed;
+
+                Vector3 bodyAssetScale = Vector3.Scale(Vector3.zero, Vector3.one);
+                float lifetime = projectile.timeToLive.f;
+                float projSpeed = projectile.movementSpeedCurrent.f;
+
+                ActionEntity parentAction = null;
+                CombatEntity bodyAssetKey = null;
+                var level = CombatComponentsLookup.Level;
+                bool bodyAssetUsed = true;
 
                 if (fragmentDelayFound && fragmentKeyFound)
                 {
                         Debug.Log("entered into first if (fragment delay and key found!)");
-
-                    if (projectile.hasParentPart)
-                    {
-                        part.GetComponent(112);
-
-                    } else if (true)
-                    {
-
-                    } 
-                    if (projectile.hasParentSubsystem)
-                    {
-                        subsystem.GetComponent(113);
-
-                    } else if (true)
-                    {
-
-                    }
-
-
+                    
                     if (projectile.flightInfo.time > fragmentDelay)
                     {
 
-                        // Destroy current projectile
+                        Debug.Log("entered into second if (missile logic). It works!");
+                        var fragmentBlueprint = DataMultiLinkerSubsystem.GetEntry(fragmentKey);
+                        var projectileData = fragmentBlueprint.projectileProcessed;
+
+                        //Destroy Current projectile
                         projectile.TriggerProjectile();
 
-                        //create new projectiles.
+                        // Create new projectile.
                         var projectileNew = combat.CreateEntity();
 
-                        //Add the new projectile and linking to the actual part and projectile data 
+                        // Attach the new projectile to the subsystem and part (parent).
                         projectileNew.AddDataLinkSubsystemProjectile(projectileData);
                         projectileNew.AddParentPart(part.id.id);
-                        projectile.AddParentSubsystem(subsystem.id.id);
 
-                        //Replace projectile properties (collision, damage and other subsystem stats.
+                        if (projectile.HasComponent(148))
+                        {
+                            projectile.ReplaceComponent(148, projectile.projectileStartPosition);
+                            projectile.GetComponent(148);
+                        }
+
+
+                        if (projectileNew.HasComponent(151))
+                        {
+                            projectile.ReplaceComponent(151, projectile.projectileTargetPosition);
+                            projectile.GetComponent(151);
+                        }
+
+                        projectile.AddParentSubsystem(subsystem.id.id);
                         projectile.ReplaceScale(bodyAssetScale);
                         projectile.ReplaceLevel(level);
 
-                        projectile.AddProjectileCollision(LayerMasks.projectileMask, 0.5f);
-                        projectile.AddInflictedDamage(2);
-                        projectile.ReplaceProjectileTargetPosition(targetPoint);
+                        projectile.AddProjectileCollision(LayerMasks.projectileMask, 0.0f);
+                        projectile.AddInflictedDamage(0);
+                        projectile.ReplaceProjectileTargetPosition(projectile.projectileTargetPosition.v.normalized);
 
-                        projectile.ReplaceMovementSpeedCurrent(1.5f);
+                        projectile.ReplaceMovementSpeedCurrent(projSpeed);
                         projectile.ReplaceRicochetChance(0);
-                        projectile.ReplaceFlightInfo(1.0f, 0.5f, firingPoint, firingPoint);
+                        projectile.ReplaceFlightInfo(0.0f, 0.0f, projectile.projectileStartPosition.v.normalized, projectile.projectileStartPosition.v.normalized);
 
                         projectile.SimpleMovement = true;
                         projectile.SimpleFaceMotion = true;
 
-                        projectile.ReplacePosition(firingPoint);
-                        projectile.ReplaceRotation(Quaternion.LookRotation(scatteredDirection.TransformDirection(2, 2, 2)));
-                        projectile.ReplaceFacing(scatteredDirection.TransformDirection(1, 1, 1));
-                        projectile.ReplaceSourceEntity(parentAction);
-                        projectile.ReplaceTimeToLive(0.0f);
+                        projectile.ReplacePosition(projectile.projectileStartPosition.v.normalized);
+                        projectile.ReplaceRotation(Quaternion.LookRotation((projectile.projectileStartPosition.v - projectile.projectileTargetPosition.v).normalized));
+                        projectile.ReplaceSourceEntity(parentAction.actionOwner.combatID);
+                        projectile.ReplaceTimeToLive(lifetime);
+
 
                         if (bodyAssetUsed)
-                            AssetPoolUtility.AttachInstance(bak.ToString(), projectile, true);
+                            AssetPoolUtility.AttachInstance(bodyAssetKey.assetKey.key, projectile, true);
 
-                        Debug.Log("entered into second if (missile logic). It works!");
+                        
                     }
                 }   
             }
