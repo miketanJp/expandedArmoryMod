4568029360d60edb476a246ed09a6f6b612acea5
Michele <eien.tabi@outlook.it>
2022-08-20 13:06:50 +0200

    Fixed component related to stats not being retrived.

--- Patch.cs
+++ Patch.cs
@@ -4,7 +4,6 @@ using Entitas;
 using PhantomBrigade;
 using PhantomBrigade.Data;
 using PhantomBrigade.Combat.Systems;
-using PhantomBrigade.Action.Components;
 
 namespace fragmentMod
 {
@@ -32,7 +31,6 @@ namespace fragmentMod
 
                 ActionEntity parentAction = null;
                 AssetLink bodyAssetKey = null;
-                var level = CombatComponentsLookup.Level;
                 bool bodyAssetUsed = true;
 
                 if (fragmentDelayFound && fragmentKeyFound)
@@ -49,19 +47,22 @@ namespace fragmentMod
                         var projectileNew = combat.CreateEntity();
 
                         // Attach the new projectile to the subsystem and part (parent).
-                        if (projectileNew.HasComponent(75)) {
-
-                            projectile.ReplaceDataLinkSubsystemProjectile(projectileData);
-                            projectileNew.GetComponent(75);
+                        if (projectileNew.HasComponent(248))
+                        {
+                            projectileNew.AddComponent(248, projectile.dataLinkSubsystemProjectile);
+                            projectileNew.AddDataLinkSubsystemProjectile(projectileData);
+                            projectileNew.GetComponent(248);
                             Debug.Log("data link added");
 
-                        } else
+                        }
+                        else
                         {
                             Debug.Log("cannot get or add/replace projectile Data.");
                         }
 
                         if (projectileNew.HasComponent(112))
                         {
+
                             projectileNew.AddParentPart(part.id.id);
                             projectileNew.GetComponent(112);
                             Debug.Log("parent part added");
@@ -73,101 +74,148 @@ namespace fragmentMod
 
                         if (projectileNew.HasComponent(113))
                         {
-                            projectileNew.AddParentSubsystem(subsystem.id.id);
-                            projectileNew.GetComponent(113);
+                            projectile.ReplaceParentSubsystem(projectile.parentSubsystem.equipmentID);
+                            projectile.GetComponent(113);
                             Debug.Log("parent subsystem added");
-                        } else
+                        }
+                        else
                         {
                             Debug.Log("cannot get or add/replace parent subsystem");
                         }
 
                         if (projectile.HasComponent(148))
                         {
-                            
+
                             projectile.ReplaceComponent(148, projectile.projectileStartPosition);
                             projectile.GetComponent(148);
-                            Debug.Log("ProjectileStartPosition added (true)");
-                        } else
+                            Debug.Log("ProjectileStartPosition added");
+                        }
+                        else
                         {
                             Debug.Log("error startPosition");
                         }
 
-                        if (projectileNew.HasComponent(151))
-                        {
-                            
-                            projectile.ReplaceComponent(151, projectile.projectileTargetPosition);
-                            projectile.GetComponent(151);
-                            Debug.Log("ProjectileTargetPosition added (true)");
-                        } else
-                        {
-                            Debug.Log("error TargetPosition");
-                        }
-
                         if (projectile.HasComponent(112))
                         {
-                            
+
                             projectile.ReplaceParentPart(projectile.id.id);
                             projectile.GetComponent(112);
                             Debug.Log("parent part added #2");
 
-                        } else
+                        }
+                        else
                         {
                             Debug.Log("cannot get or add/replace ParentPart projectile #2");
                         }
 
                         if (projectile.HasComponent(113))
                         {
-                            
+
                             projectile.ReplaceParentSubsystem(projectile.parentSubsystem.equipmentID);
                             projectile.GetComponent(113);
                             Debug.Log("parent subsystem added #2");
 
-                        } else
+                        }
+                        else
                         {
                             Debug.Log("cannot get or add/replace ParentSubsystem projectile #2");
                         }
 
-                        projectile.ReplaceScale(bodyAssetScale);
-                        projectile.ReplaceLevel(level);
+                        if (projectile.HasComponent(246))
+                        {
+                            projectile.ReplaceScale(bodyAssetScale);
+                            projectile.GetComponent(246);
+                            Debug.Log("Projectile Scale added"); 
+                        } else
+                        {
+                            Debug.Log("error Projectile scale");
+                        }
 
-                        projectile.AddProjectileCollision(LayerMasks.projectileMask, 0.0f);
+                        if (projectile.HasComponent(91))
+                        {
+                            projectile.ReplaceLevel(projectile.level.i);
+                            projectile.GetComponent(91);
+                            Debug.Log("Projectile level added");
+                        } else
+                        {
+                            Debug.Log("error projectile level");
+                        }
+
+                        if (!projectile.HasComponent(132))
+                        {
+                            projectile.AddProjectileCollision(LayerMasks.projectileMask, 1.0f);
+                            projectile.GetComponent(132);
+                            Debug.Log("Projectile collision added");
+
+                        } else
+                        {
+                            Debug.Log("error Projectile collision");
+                        }
+                        
 
                         if (projectile.HasComponent(83))
                         {
                             projectile.ReplaceInflictedDamage(projectile.inflictedDamage.f);
                             projectile.GetComponent(83);
                             Debug.Log("InflictedDamage added");
-                        } else
+                        }
+                        else
                         {
                             Debug.Log("error InflictedDamage");
                         }
 
                         if (projectile.HasComponent(151))
                         {
+
                             projectile.ReplaceProjectileTargetPosition(projectile.projectileTargetPosition.v);
                             projectile.GetComponent(151);
-                            Debug.Log("ProjectileTargetPosition added.");
-                        } else
+                            Debug.Log("ProjectileTargetPosition added #1");
+                        }
+                        else
                         {
-                            Debug.Log("error TargetPosition");
+                            Debug.Log("error TargetPosition #1");
                         }
 
-                        if (projectile.HasComponent(97))
+                        if (projectile.HasComponent(151))
                         {
-                            projectile.ReplaceMovementSpeedCurrent(0.0f);
+                            projectile.ReplaceProjectileTargetPosition(projectile.projectileTargetPosition.v);
+                            projectile.GetComponent(151);
+                            Debug.Log("ProjectileTargetPosition added #2");
+                        }
+                        else
+                        {
+                            Debug.Log("error TargetPosition #2");
+                        }
+
+                        if (projectile.HasComponent(141))
+                        {
+                            projectile.ReplaceProjectileGuidanceTargetPosition(projectile.projectileGuidanceTargetPosition.v);
+                            projectile.GetComponent(141);
+                            Debug.Log("ProjectileGuidanceTargetPosition added");
+                        }
+                        else
+                        {
+                            Debug.Log("error ProjectileGuidanceTargetPosition");
+                        }
+
+                        if (!projectile.HasComponent(97))
+                        {
+                            projectile.ReplaceMovementSpeedCurrent(1f);
                             projectile.GetComponent(97);
                             Debug.Log("MovementSpeedCurrent added");
-                        } else
+                        }
+                        else
                         {
                             Debug.Log("error MovementSpeedCurrent");
                         }
 
-                        if (projectile.HasComponent(157))
+                        if (!projectile.hasRicochetChance)
                         {
-                            projectile.ReplaceRicochetChance(projectile.ricochetChance.f);
-                            projectile.GetComponent(157);
+                            projectile.AddRicochetChance(1f);
+
                             Debug.Log("RicochetChance added");
-                        } else
+                        }
+                        else
                         {
                             Debug.Log("error RicochetChance");
                         }
@@ -177,24 +225,27 @@ namespace fragmentMod
                             projectile.ReplaceFlightInfo(0.0f, 0.0f, projectile.projectileStartPosition.v, projectile.projectileStartPosition.v);
                             projectile.GetComponent(75);
                             Debug.Log("FlightInfo added");
-                        } else
+                        }
+                        else
                         {
                             Debug.Log("error FlightInfo");
                         }
 
-                        if (projectile.HasComponent(190)) {
+                        if (!projectile.HasComponent(190))
+                        {
 
                             projectile.SimpleMovement = true;
                             projectile.GetComponent(190);
                             Debug.Log("SimpleMovement added");
-                        } else
+                        }
+                        else
                         {
                             Debug.Log("error SimpleMovement");
                         }
 
-                        if (projectile.HasComponent(188))
+                        if (!projectile.HasComponent(188))
                         {
-                            
+
                             projectile.SimpleFaceMotion = true;
                             projectile.GetComponent(188);
                             Debug.Log("SimpleFaceMotion added");
@@ -207,21 +258,23 @@ namespace fragmentMod
 
                         if (projectile.HasComponent(148))
                         {
-                            
+
                             projectile.ReplacePosition(projectile.position.v);
                             projectile.GetComponent(148);
                             Debug.Log("ProjectilePosition added");
-                        } else
+                        }
+                        else
                         {
                             Debug.Log("error ProjectilePosition");
                         }
-                        
+
                         if (projectile.HasComponent(245))
                         {
                             projectile.ReplaceRotation(projectile.rotation.q);
                             projectile.GetComponent(245);
                             Debug.Log("ProjectileRotation added");
-                        } else
+                        }
+                        else
                         {
                             Debug.Log("error ProjectileRotation");
                         }
@@ -233,15 +286,16 @@ namespace fragmentMod
                             projectile.GetComponent(198);
                             Debug.Log("SourceEntity (parentAction) Added");
 
-                        } else
+                        }
+                        else
                         {
                             Debug.Log("error replaceSourceEntity");
                         }
 
-                        if (projectile.HasComponent(209)) 
+                        if (projectile.HasComponent(209))
                         {
-                            
-                            projectile.AddTimeToLive(0.0f);
+
+                            projectile.AddTimeToLive(projectile.timeToLive.f);
                             projectile.GetComponent(209);
                             Debug.Log("HasTimeToLive Added");
                         }
@@ -251,10 +305,12 @@ namespace fragmentMod
                         }
 
 
-                        if (bodyAssetUsed) { 
+                        if (bodyAssetUsed)
+                        {
                             Debug.Log("entered into BodyAssetKey");
                             AssetPoolUtility.AttachInstance(bodyAssetKey.key, projectile, true);
-                        } else
+                        }
+                        else
                         {
                             Debug.Log("error BodyAssetKey");
                         }
@@ -262,7 +318,7 @@ namespace fragmentMod
                         Debug.Log("entered into second if (projectile logic). It works!");
 
                     }
-                }   
+                }
             }
         }
     }
